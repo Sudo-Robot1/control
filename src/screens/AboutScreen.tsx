ce0021b93f38a40923ae4ce481a8f3973b6b9a86
Pedro Silva <pedritosss444@gmail.com>
2026-03-19 22:13:38 -0300

    Telas Iniciais

---
+++ src/screens/AboutScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+
+export default function AboutScrenn(){
+    return(
+        <View>
+            <Text>Sobre </Text>
+        </View>
+    )
+}
---
+++ src/screens/RegisterScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+
+export default function RegisterScrenn(){
+    return(
+        <View>
+            <Text>Registro </Text>
+        </View>
+    )
+}
