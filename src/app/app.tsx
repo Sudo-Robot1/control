ff60205f6a35522c521227927ea655453173c4a0
Pedro Frismark <pedritosss444@gmail.com>
2026-04-08 22:18:33 -0300

    Variavel de auth e correção de home

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AppRoutes from "../routes/AppRoutes"
 
+
 export default function App(){
     return(
         <NavigationContainer>
